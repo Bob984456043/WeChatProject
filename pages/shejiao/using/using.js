919678285540eaf5523a8b8ed61fafd68751809e
Tinus <593635848@qq.com>
2019-07-07 18:32:12 +0800

    4th page modified

---
+++ pages/shejiao/using/using.js
@@ -0,0 +1,76 @@
+// pages/settings/using/using.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    appfunction:"目前ballball单词V2.1.0功能有两个:"+'\n'+
+    "1.背单词"+'\n'+
+    '选择一本书任意一课即可背单词'+'\n'+
+    "每个单元单词按照用户熟练度随机出现" + '\n' +'\n'+
+    "2.单词听写"+'\n'+
+    "听机器报听写，掏出小本本，写下自己的答案，最后对答案。",
+    appfuture:"ballball单词下一版更新如下:"+'\n'+
+    "1.单词" + '\n' +
+    '单词量将会增加新的单词库' + '\n' +'\n'+
+    "2.单词拼写" + '\n' +
+    "增加用户拼写单词以及检查的功能",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
